bb7aea3979285fceba1b02f75d760439a4077a0e
Benjamin Devèze <benjamin.deveze@gmail.com>
2018-10-31 23:59:46 +0100

    Update background color

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#e7e7e7",
+      "background_color": "#f7f7f7",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
